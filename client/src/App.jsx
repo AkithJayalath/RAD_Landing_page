156a1c0aebb34d92ad7ba800e2d66b96209ff584
AkithJayalath <akithjayalath@gmail.com>
2024-07-14 21:32:58 +0530

    created header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,10 +3,12 @@ import { BrowserRouter,Routes,Route } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import Contact from './pages/Contact'
+import Header from './components/Header'
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
