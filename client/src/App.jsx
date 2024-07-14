5b8bdce201bf287cc30fd0a61c894f5b36d309e5
AkithJayalath <akithjayalath@gmail.com>
2024-07-14 21:28:52 +0530

    created pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
 
 export default function App() {
   return (
-    <h1 className='text-3xl text-red-500'>Akith</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact-us' element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
